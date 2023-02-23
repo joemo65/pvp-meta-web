66da50517680fd60cdded8c15e14a922539e385a
Joe Moore <joemo65@gmail.com>
2023-02-22 20:28:24 -0800

    added navbar component

--- pvp-meta-web/src/App.js
+++ pvp-meta-web/src/App.js
@@ -1,18 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Navbar/Navbar.js";
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar />
+      {/* <Sidebar /> */}
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2 className="App-h2">
-          PvP Meta coming soon!
+          Coming soon!
         </h2>
         <p className="App-text">
           A place to find all of the best group comp for some of the most competitive games!
         </p>
       </header>
+    </div>
     </div>
   );
 }
